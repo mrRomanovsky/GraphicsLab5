48f87064959b3f09725b0f8878ac8f1ce267e007
mrRomanovsky <rmansorokin@gmail.com>
2018-10-12 15:25:28 +0300

    form for task 3 added

---
+++ GraphicsLab5/BezierCurves.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsLab5
+{
+    public partial class BezierCurves : Form
+    {
+        public BezierCurves()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
